a43a1b4986f60cd2a88ed7479664f180f7511b37
NiteshPrajapati <niteshkmrprajapati298@gmail.com>
2026-04-16 07:29:31 +0530

    add a console.log

--- producer.js
+++ producer.js
@@ -1,9 +1,16 @@
 const { Queue } = require('bullmq');
 const notificationQueue = new Queue('email-queue');
 async function init(params) {
+
+    console.log("Nitesh")
     const res = await notificationQueue.add('email to Nitesh', {
         email: "nitesh@applore.in", subject: "Wellcome mess", body: "Hey Nitesh Welcome to the Appp"
     })
+
+
+
+
+
     console.log("Job Added to queue", res.id);
 }
 init();
